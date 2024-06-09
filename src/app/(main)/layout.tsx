83aaefb023ea9d606de1459a358e765527f95eed
Zoody <reopard506@gmail.com>
2024-06-09 17:56:12 +0900

    [🎨STYLE]     : App 심볼 디자인
       - App 심볼 디자인 스타일

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,9 +2,11 @@ import ModeToggle from "@/components/providers/ModeToggle";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex md:mx-w-2xl lg:mx-w-4xl mx-auto">
+    <div className="relative h-full flex md:mx-w-2xl lg:mx-w-4xl mx-auto">
       <main className="flex-1 h-full w-full overflow-y-auto">{children}</main>
-      <ModeToggle />
+      <div className="absolute top-2 right-2">
+        <ModeToggle />
+      </div>
     </div>
   );
 };
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,18 +1,18 @@
 const MainPage = () => {
   return (
-    <div className="h-full flex justify-center items-center gap-x-12">
-      <div className="flex flex-col">
-        <div className="w-[5rem] h-[5rem] rounded-full bg-symbol-500" />
-        <div className="w-[5rem] h-[5rem] rounded-full bg-symbol-300" />
-        <div className="w-[5rem] h-[5rem] rounded-full bg-symbol-100" />
-        <div className="w-[5rem] h-[5rem] rounded-full bg-primary" />
+    <div className="h-full flex justify-center gap-x-28">
+      <div className="flex flex-col mt-[10rem] relative w-[5rem]">
+        <div className="absolute w-[6rem] h-[6rem] rounded-full bg-symbol-500 top-0" />
+        <div className="absolute w-[6rem] h-[6rem] rounded-full bg-symbol-300 top-[3rem]" />
+        <div className="absolute w-[6rem] h-[6rem] rounded-full bg-symbol-100 top-[6rem]"  />
+        <div className="absolute w-[6rem] h-[6rem] rounded-full bg-primary top-[9rem]" />
       </div>
-      <div className="flex flex-col items-center">
-        <p className="text-symbol-500 font-Pacifico text-[10rem]">Recipe</p>
+      <div className="flex flex-col items-center relative">
+        <p className="text-symbol-500 font-Pacifico text-[10rem] pt-[10rem]">Recipe</p>
         <p className="text-symbol-500 font-Kenwave text-[1.5rem] mt-6">
           Portfolio & Devlog
         </p>
-        <p className="font-Libre_Barcode_39 mt-10 text-6xl text-primary">
+        <p className="absolute font-Libre_Barcode_39 text-6xl text-primary bottom-10 right-0">
           Recipe
         </p>
       </div>
