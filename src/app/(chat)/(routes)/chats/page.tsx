47ba19a1d8fed03df063e74cabd2a15a4059e1fb
오원주 <reopard506@gmail.com>
2024-07-02 15:37:29 +0900

    Release/1.1.2 (#14)
    * [🎨STYLE]     : 채팅 관련 스타일 조정 및 chats 페이지 인증 여부에 따른 UI 변경 (#12)
    
    - editor background highlight gray 색상 가시성 개선
    
    * [🔨CHORE]     : release 버전 명시

--- src/app/(chat)/(routes)/chats/page.tsx
+++ src/app/(chat)/(routes)/chats/page.tsx
@@ -3,14 +3,29 @@
 import { PlusCircle } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import useAuthStore from "@/store/useAuthsStore";
+import { useModal } from "@/store/useModalStore";
+import { useEffect } from "react";
 
 const ChatsPage = () => {
+  const { auth } = useAuthStore();
+  const { onOpen } = useModal();
+
+  useEffect(() => {
+    if (typeof auth !== "undefined" && !auth) onOpen("enterChat");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [auth]);
+
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
-      <Button>
-        <PlusCircle className="h-4 w-4 mr-2" />
-        Create a Chat
-      </Button>
+      {!auth ? (
+        <Button onClick={() => onOpen("enterChat")}>채팅 입장하기</Button>
+      ) : (
+        <Button onClick={() => onOpen("createChat")}>
+          <PlusCircle className="h-4 w-4 mr-2" />
+          Create a Chat
+        </Button>
+      )}
     </div>
   );
 };
