aaab96fc5c0f8d48883a0ff3be80c575d62cdfe3
Zoody <reopard506@gmail.com>
2024-08-03 13:32:56 +0900

    [🐛FIX]       : 배포시 Export encountered errors on following paths 문제 수정

--- src/app/(blog)/(route)/blog/(tabs)/coureses/page.tsx
+++ src/app/(blog)/(route)/blog/(tabs)/coureses/page.tsx
@@ -1,46 +1,15 @@
-"use client";
-
-import { Card } from "@/app/(blog)/_components";
-import { Skeleton } from "@/components/ui/skeleton";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import { Series } from "prisma/prisma-client";
 import React, { Suspense } from "react";
+
+import { CouresesSection } from "@/app/(blog)/_components";
+
 import LoadingPage from "../loading";
 
 export const dynamic = "force-dynamic";
 
 const CoursePage = () => {
-  const { data, isLoading } = useQuery({
-    queryKey: ["all-serieses"],
-    queryFn: async () => await axios.get("/api/series?simple"),
-  });
-  const seriesList = data?.data as Series[];
-
   return (
     <Suspense fallback={<LoadingPage />}>
-      <section className="flex flex-col mb-24">
-        <div className="flex-1 grid md:grid-cols-3 lg:grid-cols-4 gap-3 h-full">
-          {isLoading &&
-            Array(8)
-              .fill(0)
-              .map((_, i) => (
-                <Skeleton
-                  key={i}
-                  className="rounded-lg w-full aspect-[3/2] md:aspect-[3/4]"
-                />
-              ))}
-          {!isLoading &&
-            seriesList?.map((series: Series) => (
-              <Card
-                key={series.id}
-                id={series.id}
-                cardType="series"
-                title={series.name}
-              />
-            ))}
-        </div>
-      </section>
+      <CouresesSection />
     </Suspense>
   );
 };
---
+++ src/app/(blog)/_components/CoursesSection.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { Series } from "prisma/prisma-client";
+
+import { Skeleton } from "@/components/ui/skeleton";
+
+import { Card } from "@/app/(blog)/_components";
+
+const CouresesSection = () => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["all-serieses"],
+    queryFn: async () => await axios.get("/api/series?simple"),
+  });
+  const seriesList = data?.data as Series[];
+
+  return (
+    <section className="flex flex-col mb-24">
+      <div className="flex-1 grid md:grid-cols-3 lg:grid-cols-4 gap-3 h-full">
+        {isLoading &&
+          Array(8)
+            .fill(0)
+            .map((_, i) => (
+              <Skeleton
+                key={i}
+                className="rounded-lg w-full aspect-[3/2] md:aspect-[3/4]"
+              />
+            ))}
+        {!isLoading &&
+          seriesList?.map((series: Series) => (
+            <Card
+              key={series.id}
+              id={series.id}
+              cardType="series"
+              title={series.name}
+            />
+          ))}
+      </div>
+    </section>
+  );
+};
+
+export default CouresesSection;
