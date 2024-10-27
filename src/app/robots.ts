f87a3388a3899efed41bd8e7a50ad5501fc490b3
Zoody <reopard506@gmail.com>
2024-10-27 16:28:05 +0900

    [🐛FIX]       : 로봇 배제 표준 수정 - 불허 경로 수정

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -8,7 +8,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/private/",
+      disallow: "/chats/",
     },
     sitemap: `${BASE_URL}/sitemap.xml`,
   };
