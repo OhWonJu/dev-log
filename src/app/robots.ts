057e35599899b90160235643b08ed81a3771c82a
Zoody <reopard506@gmail.com>
2024-11-26 00:46:22 +0900

    [🐛FIX]       : robots disallow 추가    - blog/search

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -8,7 +8,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/chats/",
+      disallow: ["/chats/", "/blog/search"],
     },
     sitemap: `${BASE_URL}/sitemap.xml`,
   };
